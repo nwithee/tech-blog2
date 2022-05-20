c146b31720b7957a6f968e5ec4dc9690a1ee87f3
nzwithee@gmail.com <Cartoon#01>
2022-05-19 20:20:36 -0500

    b

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const { User, Post, Comment } = require('../../models');
 
 router.post('/', (req, res) => {
   // Create new user
