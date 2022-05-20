8cc17e944cc222c3ca2fda1e40eecdbd6b505fd7
nzwithee@gmail.com <Cartoon#01>
2022-05-19 20:13:47 -0500

    temp fix for heroku?

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 //const { User, Post, Comment } = require('../../models');
-const { Post, User, Comment} = require('../../models');
-
 
 router.post('/', (req, res) => {
   // Create new user
