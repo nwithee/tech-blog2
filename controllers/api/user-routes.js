b3ad72911912a2d7dcc1a53608eb9011aa9a5bc1
nzwithee@gmail.com <Cartoon#01>
2022-05-19 19:55:14 -0500

    heroku updates

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+//const { User, Post, Comment } = require('../../models');
+const { Post, User, Comment} = require('../../models');
 
 
 router.post('/', (req, res) => {
